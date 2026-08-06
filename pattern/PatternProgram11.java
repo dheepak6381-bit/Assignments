6f3473b9ca365e00d2e0c706b29af4c7fd9df2a9
DHEEPAK S <dheepak6381@gmail.com>
2026-08-06 14:59:35 +0530

    Delete pattern directory

--- pattern/PatternProgram11.java
+++
@@ -1,5 +0,0 @@
-﻿public class PatternProgram11 {
-    public static void main(String[] args) {
-        System.out.println("Pattern Program 11");
-    }
-}
--- pattern/PatternProgram12.java
+++
@@ -1,5 +0,0 @@
-﻿public class PatternProgram12 {
-    public static void main(String[] args) {
-        System.out.println("Pattern Program 12");
-    }
-}
